318c52a4beb8030586732d145cbc8f92485ffd5d
1351618 <1351618@gmail.com>
2024-02-28 12:35:46 +0200

    div_video

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const start = document.getElementById("start");
+const div_video = document.querySelector(".div-video");
 
 start.addEventListener("click", () => {
   console.log("start");
@@ -7,4 +8,13 @@ start.addEventListener("click", () => {
 
 function playWind() {
   console.log("play");
+  div_video.classList.remove("hide");
+  const videoPlayer = document.getElementById("video-player");
+  videoPlayer.addEventListener("ended", handleVideoEnded);
+  videoPlayer.play();
+}
+
+function handleVideoEnded() {
+  console.log("Видео окончено");
+  div_video.classList.add("hide");
 }
