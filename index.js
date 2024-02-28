bc8a205ccd9862977985d9d3128c1242364e543f
1351618 <1351618@gmail.com>
2024-02-28 19:58:17 +0200

    finish

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
+const startVideo = "КОГДА У NPC ПЕРЕРЫВ";
+const playListData = {
+  trec_1: "КОГДА У NPC ПЕРЕРЫВ",
+  trec_2: "КОГДА ДУМАЕШЬ ЧТО ТВОЙ АЙТИШНИК ТЕЛЕПАТ (1)",
+};
+
 const start = document.getElementById("start");
+const div_start = document.querySelector(".start");
+const div_winPlayList = document.querySelector(".win-playlist");
 const div_video = document.querySelector(".div-video");
+const div_playlist = document.querySelector(".playlist");
 
 start.addEventListener("click", () => {
   console.log("start");
-  playWind();
+  const videoPath = startVideo;
+  playWind(videoPath);
 });
 
-function playWind() {
+function playWind(videoPath) {
   console.log("play");
   div_video.classList.remove("hide");
   const videoPlayer = document.getElementById("video-player");
-  videoPlayer.addEventListener("ended", handleVideoEnded);
+  videoPlayer.src = `./video/${videoPath}.mp4`;
+
+  videoPlayer.addEventListener("ended", () => {
+    console.log("Видео закончилось");
+    div_video.classList.add("hide");
+    div_start.classList.add("hide");
+    div_winPlayList.classList.remove("hide");
+  });
+
   videoPlayer.play();
 }
 
-function handleVideoEnded() {
-  console.log("Видео окончено");
-  div_video.classList.add("hide");
+// добавляем на страницу
+for (let key in playListData) {
+  console.log(key);
+  let trecTitlt = document.createElement("p");
+  trecTitlt.textContent = playListData[key];
+  trecTitlt.addEventListener("click", () => {
+    console.log(playListData[key]);
+    playWind(playListData[key]);
+  });
+  div_playlist.appendChild(trecTitlt);
 }
