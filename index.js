e36a884bc849b221edd3f307847f13d4f52db854
1351618 <1351618@gmail.com>
2024-02-28 22:35:30 +0200

    kjshk

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const startVideo = "КОГДА У NPC ПЕРЕРЫВ";
+const startVideo = "./video/КОГДА У NPC ПЕРЕРЫВ.mp4";
 const playListData = {
-  trec_1: "атестат",
-  trec_2: "запахло весной",
-  trec_3: "3 сентября",
+  trec_1: "./video/атестат.mp4",
+  trec_2: "./video/запахло весной.mp4",
+  trec_3: "./video/3 сентября.mp4",
+  trec_4:
+    "https://drive.google.com/file/d/1iYbgfLzSIR1-PFahx01a-6Qlvj9GS8CJ/view?usp=drive_link",
 };
 
 const start = document.getElementById("start");
@@ -21,7 +23,7 @@ function playWind(videoPath) {
   console.log("play");
   div_video.classList.remove("hide");
   const videoPlayer = document.getElementById("video-player");
-  videoPlayer.src = `./video/${videoPath}.mp4`;
+  videoPlayer.src = videoPath;
 
   videoPlayer.addEventListener("ended", () => {
     console.log("Видео закончилось");
